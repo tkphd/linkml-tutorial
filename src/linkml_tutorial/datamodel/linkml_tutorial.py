8e325f3f51e10f33af7309ab59c04410892d125f
Trevor Keller <trevor.keller@nist.gov>
2023-03-09 00:03:16 -0500

    Refresh generated contents (make all)

--- src/linkml_tutorial/datamodel/linkml_tutorial.py
+++ src/linkml_tutorial/datamodel/linkml_tutorial.py
@@ -1,5 +1,5 @@
 # Auto generated from linkml_tutorial.yaml by pythongen.py version: 0.9.0
-# Generation date: 2023-03-08T23:07:40
+# Generation date: 2023-03-09T00:03:27
 # Schema: linkml-tutorial
 #
 # id: https://w3id.org/tkphd/linkml-tutorial
